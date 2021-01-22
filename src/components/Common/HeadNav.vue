9c1b703e777cfbe32ebe9d15877a09396b2a48aa
zf123456-1 <77775638+zf123456-1@users.noreply.github.com>
2021-01-22 10:57:54 +0800

    Update HeadNav.vue

--- src/components/Common/HeadNav.vue
+++ src/components/Common/HeadNav.vue
@@ -5,7 +5,7 @@
         <el-col :span="1" :offset="1"><el-image :src="logoImg"  fit="fill" height="60px"></el-image></el-col>
         <el-col :span="3" ><div class="name">中信书店</div></el-col>
         <el-col :span="10" :offset="9" class="search">
-          <el-input placeholder="书名、作者、出版社、ISBN" v-model="input" class="input-with-select" style="width: 500px;height: 40px;">
+          <el-input placeholder="书名、作者、出版社、ISBN" v-model="input" class="input-with-select" style="width: 500px;height: 40px;" @keyup.enter.native="searchBook(input)">
             <el-select v-model="select" slot="prepend" placeholder="请选择">
               <el-option label="书名" value="1"></el-option>
               <el-option label="作者" value="2"></el-option>
